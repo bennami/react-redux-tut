d24aa8a1196b469644ad93afc54495387f08952e
imane <benbenimane@hotmail.com>
2020-05-01 01:59:11 +0200

    added files for local api config from course, created first thunks, actions and reducers, spend too much time looking for a json error

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-
 
 const AboutPage = () => (
     <div className={'jumbotron'}>
         <h2>About</h2>
-
-
         <p>Lorem ipsum dolor sit amet.</p>
     </div>
 
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,68 +1,75 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import * as courseActions from '../../redux/actions/courseActions'
+import * as authorActions from '../../redux/actions/authorActions'
 import PropTypes from 'prop-types'
 import {bindActionCreators} from "redux";
+import CourseList from "./CourseList";
+
 
 class CoursesPage extends React.Component {
 
-    state ={
-        course:{
-            title:''
+    componentDidMount() {
+        const { courses, authors, actions } = this.props;
+
+        if (courses.length === 0) {
+            actions.loadCourses().catch(error => {
+                alert("Loading courses failed" + error);
+            });
         }
-    };
 
-    /*    this.handleChange = this.handleChange.bind(this);*/
-    //arrow functions replace this inside a constructor
-
-
-handleChange = (e) =>{
-    const course={ ...this.state.course, title: e.target.value};
-    this.setState({course:course});
-};
-
-handleSubmit =(e)=>{
-    e.preventDefault();
-    this.props.actions.createCourse(this.state.course);
+        if (authors.length === 0) {
+            actions.loadAuthors().catch(error => {
+                alert("Loading authors failed" + error);
+            });
+        }
+    }
 
 
-};
-
-
-
-render() {
+    render() {
     return (
-        <form onSubmit={this.handleSubmit}>
+          <>
             <h2>Courses</h2>
-            <h3>Add course</h3>
-            <input type={'text'} onChange={this.handleChange.bind(this)} value={this.state.course.title}/>
-            <input type={'submit'} value={'save'}/>
+              <CourseList courses={this.props.courses}/>
             {this.props.courses.map(course =>(
                 <div key={course.title}>{course.title}</div>
             ))}
-        </form>
-    );
+          </>
+            );
   }
 }
 
-CoursesPage.propTypes ={
+CoursesPage.propTypes = {
+    authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
-    actions:PropTypes.object.isRequired
+    actions: PropTypes.object.isRequired
 };
 
-
 function mapStateToProps(state) {
-    return{
-        courses: state.courses
-    }
-
+    return {
+        courses:
+            state.authors.length === 0
+                ? []
+                : state.courses.map(course => {
+                    return {
+                        ...course,
+                        authorName: state.authors.find(a => a.id === course.authorId).name
+                    };
+                }),
+        authors: state.authors
+    };
 }
 
 function mapDispatchToProps(dispatch) {
     return{
-       actions: bindActionCreators(courseActions, dispatch)
-    }
-
+       actions:{
+        loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+        loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+       }
+    };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps) (CoursesPage);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CoursesPage);
