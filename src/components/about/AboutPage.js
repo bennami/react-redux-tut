ba4f1a97dc157d5347e83d3e3f4bb4ebff77c30d
imane <benbenimane@hotmail.com>
2020-04-30 23:48:39 +0200

    react redux course, making dev environment from scratch, setting up router and creating first redux flow env, using a mock api

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+
+const AboutPage = () => (
+    <div className={'jumbotron'}>
+        <h2>About</h2>
+
+
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+
+
+);
+export default AboutPage
+
+
+
+
---
+++ src/components/common/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Header = () =>{
+
+const activeStyle = {color: '#F15B2A'};
+
+
+    return(
+        <nav>
+            <NavLink   to={'/'} activeStyle={activeStyle} exact>home </NavLink>
+            {'|'}
+            <NavLink   to={'/about'} activeStyle={activeStyle} exact> about</NavLink>
+            {'|'}
+            <NavLink   to={'/courses'} activeStyle={activeStyle} exact> courses</NavLink>
+        </nav>
+    )
+};
+export default Header
